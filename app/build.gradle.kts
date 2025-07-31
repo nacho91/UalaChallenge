ca76bad62f69997d7695df2033ce1a67e3190b6b
Ignacio Oviedo <oviedoignacio91@gmail.com>
2025-07-30 23:22:04 -0300

    Initial UI structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
 
     buildTypes {
         release {
+            isMinifyEnabled = true
             isShrinkResources = true
-            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
---
+++ app/src/main/java/com/nacho/uala/challenge/UalaChallengeApplication.kt
@@ -0,0 +1,7 @@
+package com.nacho.uala.challenge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class UalaChallengeApplication : Application()
